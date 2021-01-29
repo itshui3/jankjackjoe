88b919975c036b731f327178b5d40ae366062301
Hui <xuhuizhu369@gmail.com>
2021-01-29 04:31:24 -0800

    draws cross

--- src/components/Body/SVG/Cross/Cross.js
+++ src/components/Body/SVG/Cross/Cross.js
@@ -4,9 +4,15 @@ import React from 'react'
 function Cross() {
 return (
 <>
-    <svg width="95%" height="95%">
-        <line></line>
-        <line></line>
+    <svg width="25%" height="25%">
+        <line stroke="black" strokeWidth="1.5"
+        x1="0%" y1="0%"
+        x2="100%" y2="100%"
+        />
+        <line stroke="black" strokeWidth="1.5"
+        x1="100%" y1="0%"
+        x2="0%" y2="100%"
+        />
     </svg>
 </>
 )
