5b48f51247069d2989df3eb09f8f79277b5e3a7c
Hui <xuhuizhu369@gmail.com>
2021-02-03 04:25:57 -0800

    reset board, drafts plan in comments, init and import pieces

--- src/components/Body/SVG/index.js
+++
@@ -1,8 +0,0 @@
-
-import Cross from './Cross/Cross'
-import Circle from './Circle/Circle'
-
-export {
-    Cross,
-    Circle
-}
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,30 @@
 
-import Header from './Header/Header'
-import Body from './Body/Body'
+import Header from './Header/Header.js'
+import Board from './Board/Board.js'
 
 export {
     Header,
-    Body
+    Board
 }
+
+/*
+Header -
+TicTacToe
+
+Game State Info
+
+start game - button
+clicking on this will give the turn to a player thus enabling them to select a tile to place their piece
+
+P1 - text
+indicates it is player 1's turn, clicks on empty tiles will generate their piece and give the turn to player 2
+
+P2 - text
+indicates it is player 2's turn ,clicks on empty tiles will generate their piece and give the turn to player 1
+
+GameOver[Click anywhere to restart] - text
+if there are 3 of the same pieces in a row, the game will end and render text stating which player won
+I will try to do this functionally, giving GameOver text an interpolation property such that 3-piece validator can pass winner-text to gameOver's state to be interpolated in. 
+In this method, we minimize the amount of state needing to be tracked and compose our logic functionally
+
+*/
